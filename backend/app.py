3f4a133857eb312b652365399e0246be9bf27292
Dan Calacci <dcalacci@media.mit.edu>
2021-07-23 12:11:34 -0400

    Adds dockerfile and remove static path from flask
    - changes mapbox.gl version to fix https://github.com/visgl/react-map-gl/issues/1266
    - Adds mapbox styles to index.html

--- backend/app.py
+++ backend/app.py
@@ -13,8 +13,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from database import db
 
-app = Flask(__name__, static_url_path="/static",
-            static_folder='./static/static')
+app = Flask(__name__)
 app.config.from_object(Config)
 #db = SQLAlchemy(app)
 db.init_app(app) 
