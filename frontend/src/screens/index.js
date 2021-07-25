d5e5387f728286abe63ae5ff4c7df8a66cd6aea3
aberke <berke.alexandra@gmail.com>
2021-07-25 15:25:37 -0400

    moved remaining screens to /screens

--- frontend/src/screens/index.js
+++ frontend/src/screens/index.js
@@ -1,2 +1,5 @@
 export { default as NewAsk} from "./NewAsk";
 export { default as Ask} from "./Ask";
+export { default as About} from "./About";
+export { default as MyStuff} from "./MyStuff";
+export { default as Home} from "./Home";
